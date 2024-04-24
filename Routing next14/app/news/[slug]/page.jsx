2445ece1a71ab096930b4ad59bfe9024c704b436
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-24 18:41:20 +0200

    Nested Routes Inside Dynamic Routes display  img

--- Routing next14/app/news/[slug]/page.jsx
+++ Routing next14/app/news/[slug]/page.jsx
@@ -1,5 +1,6 @@
 //... id page
 import { DUMMY_NEWS } from "@/Dummy-news";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -7,13 +8,15 @@ function NewsDetailPage({ params }) {
   const newsSlug = params.slug;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
 
-  if(!newsItem){
-    notFound()
+  if (!newsItem) {
+    notFound();
   }
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
         <h1>{newsItem?.title}</h1>
         <time dateTime={newsItem?.data}>{newsItem?.date}</time>
       </header>
