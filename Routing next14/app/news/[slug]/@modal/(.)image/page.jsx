cc64396b5164ec55b406e5a5bfaaffa7da5e775e
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-24 19:17:32 +0200

    Combining Parallel & Intercepting Routes
     modal

--- Routing next14/app/news/[slug]/@modal/(.)image/page.jsx
+++ Routing next14/app/news/[slug]/@modal/(.)image/page.jsx
@@ -1,8 +1,8 @@
 //elfogo utvonal, a () elérési utvonala van jelenleg a képnek zárójel után maga a mappa név
-
 import { DUMMY_NEWS } from "@/Dummy-news";
+import { notFound } from "next/navigation";
 
-export default function InterCeptedImagePage({ params }) {
+export default function InterceptedImagePage({ params }) {
   const newsItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find(
     (newsItem) => newsItem.slug === newsItemSlug
@@ -11,12 +11,15 @@ export default function InterCeptedImagePage({ params }) {
   if (!newsItem) {
     notFound();
   }
+
   return (
     <>
-    <h2>Intercepted</h2>
-      <div className="fullscreen-image">
-        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-      </div>
+      <div className="modal-backdrop" />
+      <dialog className="modal" open>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </div>
+      </dialog>
     </>
   );
 }
