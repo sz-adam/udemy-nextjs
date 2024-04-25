62fc8c473adfe7b2c9b851f53a42b663e4c273a5
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-25 18:15:12 +0200

    Server-side Data Fetching

--- Data-fetching-next-14/app/(content)/news/page.jsx
+++ Data-fetching-next-14/app/(content)/news/page.jsx
@@ -1,47 +1,17 @@
 //news page
-"use client";
 import NewList from "@/components/NewList";
-import { useEffect, useState } from "react";
 
-function NewsPage() {
-  const [isloading, setIsloading] = useState(false);
-  const [error, setError] = useState();
-  const [news, setNews] = useState();
-
-  useEffect(() => {
-    async function fetchNews() {
-      setIsloading(true);
-      const response = await fetch('http://localhost:8080/news');
-
-      if (!response.ok) {
-        setError("Error fetching news");
-        setIsloading(false);
-      }
-
-      const news = await response.json();
-      setIsloading(false);
-      setNews(news);
-    }
-    fetchNews();
-  }, []);
-
-  if (isloading) {
-    return <p>Loading...</p>;
+async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
   }
-
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-  if (news) {
-    newsContent = <NewList news={news} />;
-  }
+  const news = await response.json();
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewList news={news} />
     </>
   );
 }
