c4df99653af141882fbb4c09899d87b3a02bb44b
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-25 18:07:25 +0200

    Client-side Data Fetching

--- Data-fetching-next-14/app/(content)/news/page.jsx
+++ Data-fetching-next-14/app/(content)/news/page.jsx
@@ -1,13 +1,47 @@
 //news page
-
-import { DUMMY_NEWS } from "@/Dummy-news";
+"use client";
 import NewList from "@/components/NewList";
+import { useEffect, useState } from "react";
 
 function NewsPage() {
+  const [isloading, setIsloading] = useState(false);
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsloading(true);
+      const response = await fetch('http://localhost:8080/news');
+
+      if (!response.ok) {
+        setError("Error fetching news");
+        setIsloading(false);
+      }
+
+      const news = await response.json();
+      setIsloading(false);
+      setNews(news);
+    }
+    fetchNews();
+  }, []);
+
+  if (isloading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+  if (news) {
+    newsContent = <NewList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-        <NewList news={DUMMY_NEWS}/>
+      {newsContent}
     </>
   );
 }
