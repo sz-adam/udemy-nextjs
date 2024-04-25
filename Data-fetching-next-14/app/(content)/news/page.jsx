6eaa6bf42517d67afdc0673a167d446c573a1a23
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-25 18:28:13 +0200

    Fetching Directly From The Source!

--- Data-fetching-next-14/app/(content)/news/page.jsx
+++ Data-fetching-next-14/app/(content)/news/page.jsx
@@ -1,12 +1,9 @@
 //news page
 import NewList from "@/components/NewList";
+import { getAllNews } from "@/lib/news";
 
 async function NewsPage() {
-  const response = await fetch("http://localhost:8080/news");
-  if (!response.ok) {
-    throw new Error("Failed to fetch news");
-  }
-  const news = await response.json();
+const news =getAllNews()
 
   return (
     <>
