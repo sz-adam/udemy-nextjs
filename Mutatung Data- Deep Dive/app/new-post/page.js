f4bb3698b7fd9025a19a80413712a9c6d0809d0f
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-26 19:59:21 +0200

    Adjusting Server Actions for useFormState (create errors , useFormState use client )

--- Mutatung Data- Deep Dive/app/new-post/page.js
+++ Mutatung Data- Deep Dive/app/new-post/page.js
@@ -1,50 +1,42 @@
-import FormSubmit from '@/components/FormSubmit';
-import { storePost } from '@/lib/posts';
-import { redirect } from 'next/navigation';
-
-
+import PostForm from "@/components/PostForm";
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
+  async function createPost(prevState, formData) {
     "use server";
-    const title = formData.get('title');
-    const image = formData.get('image');
-    const content = formData.get('content');
+    const title = formData.get("title");
+    const image = formData.get("image");
+    const content = formData.get("content");
+
+    let errors = [];
+
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required.");
+    }
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required.");
+    }
+    if (!image || image.size === 0) {
+      errors.push("Image is required.");
+    }
+
+    if (errors.length > 0) {
+      return {errors}
+    }
 
     await storePost({
-      imageUrl: '',
+      imageUrl: "",
       title,
       content,
-      userId: 1
+      userId: 1,
     });
-    redirect('/feed')
+    redirect("/feed");
   }
+  
+
 
   return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-         <FormSubmit />
-        </p>
-      </form>
-    </>
+    <PostForm action={createPost}/>
   );
 }
