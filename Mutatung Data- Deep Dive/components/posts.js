472076e488979110e6eafd72400a8775db2fb40b
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-26 20:08:29 +0200

    typo ;

--- Mutatung Data- Deep Dive/components/posts.js
+++ Mutatung Data- Deep Dive/components/posts.js
@@ -30,7 +30,7 @@ function Post({ post }) {
 
 export default function Posts({ posts }) {
   if (!posts || posts.length === 0) {
-    return <p>There are no posts yet. Maybe start sharing some?</p>;
+    return <p>There are no posts yet. Maybe start sharing some?</p>
   }
 
   return (
