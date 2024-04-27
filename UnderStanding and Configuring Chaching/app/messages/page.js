56ea185e5bec1d4e7527b6d3712bc0947a9a8960
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-27 15:06:51 +0200

    Controlling Data Caching

--- UnderStanding and Configuring Chaching/app/messages/page.js
+++ UnderStanding and Configuring Chaching/app/messages/page.js
@@ -1,11 +1,20 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
+import { unstable_noStore } from "next/cache";
+
+//x másodpercenként törli a gyorsítótárat és ujból kekéri az adatokat  
+//revalidate le foglalt név ezt keresi a nextjs
+//export const revalidate = 1000;
+
+//export const dynamic ='force-dynamic' //ugyanaz mint a cache:'no-store'
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
+  //unstable_noStore();
+  const response = await fetch("http://localhost:8080/messages");
+  // cache:'no-store' nem menti a gyorsítotárba igy mindig ujra lekéri az adatokat
+
+  //next:{ 5 másodpercenként törli a gyorsítótárat és ujból kekéri az adatokat
+  //  revalidate:5
+  // }
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
