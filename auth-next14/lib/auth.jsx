9e58a2d91336c0f17ecff677c21180c211396457
sz-adam <112242221+sz-adam@users.noreply.github.com>
2024-04-28 17:19:50 +0200

    Configuring A Session & A Session Cookie

--- auth-next14/lib/auth.jsx
+++ auth-next14/lib/auth.jsx
@@ -2,6 +2,7 @@ import { Lucia } from 'lucia';
 import { BetterSqlite3Adapter } from '@lucia-auth/adapter-sqlite'
 
 import db from './db';
+import { cookies } from 'next/headers';
 
 const adapter = new BetterSqlite3Adapter(db, {
   user: 'users',
@@ -16,3 +17,13 @@ const lucia = new Lucia(adapter, {
     }
   }
 });
+
+export async function createAuthSession(userId) {
+    const session = await lucia.createSession(userId, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+  }
